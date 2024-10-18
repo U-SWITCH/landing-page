e55d05068026722ed79fb607a8ead0dc060371c2
KostLinux <KostLinux@users.noreply.github.com>
2024-10-18 08:01:35 +0000

    deploy: a85c7021150f3d80ae3bd0147f4f1f781402f7b0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,23 @@
-document.getElementById('menu-button').addEventListener('click', function() {
-    var menu = document.getElementById('mobile-menu');
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-        setTimeout(function() {
-            menu.classList.add('show');
-        }, 10); // Small delay to ensure the transition works
-    } else {
-        menu.classList.remove('show');
-        setTimeout(function() {
-            menu.classList.add('hidden');
-        }, 300); // Delay to match the transition duration
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    // Mobile menu toggle
+    document.getElementById('menu-button').addEventListener('click', function() {
+        var menu = document.getElementById('mobile-menu');
+        if (menu.classList.contains('hidden')) {
+            menu.classList.remove('hidden');
+            setTimeout(function() {
+                menu.classList.add('show');
+            }, 10); // Small delay to ensure the transition works
+        } else {
+            menu.classList.remove('show');
+            setTimeout(function() {
+                menu.classList.add('hidden');
+            }, 300); // Delay to match the transition duration
+        }
+    });
+
+    // Calendly popup widget
+    document.getElementById('schedule-button').addEventListener('click', function() {
+        Calendly.initPopupWidget({ url: 'https://calendly.com/apilookinlabs/30min' });
+        return false;
+    });
 });
